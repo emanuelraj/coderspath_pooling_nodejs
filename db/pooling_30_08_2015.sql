38afe47cada82b6dee24b789ecbb9ed646464ca5
mathan@cumulations.com <gurumathan>
2015-09-06 16:04:24 +0530

    Updated Socketio changes and DB changes

---
+++ db/pooling_30_08_2015.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 25, 2015 at 04:55 PM
+-- Server version: 5.6.12-log
+-- PHP Version: 5.4.12
+
+SET SQL_MODE = "ALLOW_INVALID_DATES";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `pooling`
+--
+CREATE DATABASE IF NOT EXISTS `pooling` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `pooling`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chat`
+--
+
+CREATE TABLE IF NOT EXISTS `chat` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `conversation_id` bigint(11) DEFAULT NULL,
+  `sender_id` bigint(11) DEFAULT NULL,
+  `message_type` bigint(11) NOT NULL,
+  `message` text NOT NULL,
+  `sent_at` datetime DEFAULT NULL,
+  `delivered_at` datetime DEFAULT NULL,
+  `seen_at` datetime DEFAULT NULL,
+  `created_at` TIMESTAMP ,
+  `modified_at` TIMESTAMP ,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `conversation_user_mapping`
+--
+
+CREATE TABLE IF NOT EXISTS `conversation_user_mapping` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `from_id` bigint(11) NOT NULL,
+  `to_id` bigint(11) NOT NULL,
+  `created_at` TIMESTAMP ,
+  `modified_at` TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `socket_session_id` varchar(50) DEFAULT NULL,
+  `created_at` TIMESTAMP ,
+  `modified_at` TIMESTAMP ,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
