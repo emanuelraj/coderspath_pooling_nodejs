4153ec5f2c04ba0ad99f7e944c512927392ff0c7
allenGift <raju.allen1888@gmail.com>
2015-09-10 16:16:54 +0530

    Remort Port1

--- chat.js
+++ chat.js
@@ -14,8 +14,8 @@ var app = require('http').createServer(),
     port: 3306
   });
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 
 connection.connect(function(err) {
